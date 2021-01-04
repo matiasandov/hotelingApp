a55186ec1539e1fb3250738d94bbbb0dc8cdb173
Matias <matt.sandov@gmail.com>
2021-01-04 16:04:22 -0600

    se crearon paginas para authenticacion y para ver los lugares que tiene el usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
 ];
 
 @NgModule({
